d32dda6e768c7e72bf7ceac1d535ef6d1829f1a9
MK <mertkoksal88@gmail.com>
2020-04-07 13:18:17 +0300

    Initial Commit

---
+++ pokedex3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pokedex3
+//
+//  Created by Mert Köksal on 7.04.2020.
+//  Copyright © 2020 Mert Köksal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
